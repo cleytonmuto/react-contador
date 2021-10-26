2642b5bb93a3f07559b12f1b7266cd0421bc749d
cleytonmuto <cleytonmuto@gmail.com>
2021-10-26 20:59:32 -0300

    Initial commit form home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+
+  const handleSubtractOne = () => {
+    setCount(count - 1);
+  };
+
+  const handleAddOne = () => {
+    setCount(count + 1);
+  };
+
+  const handleOnChange = (e) => {
+    setQuantity(e.target.value);
+  };
+
+  const handleSubtractQuantity = () => {
+    if (quantity) setCount(count - parseInt(quantity, 10));
+  };
+
+  const handleAddQuantity = () => {
+    if (quantity) setCount(count + parseInt(quantity, 10));
+  };
+
+  const handleResetCounter = () => {
+    setCount(0);
+    setQuantity(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      id={"content-wrapper"}
+      className={"flex column vertical-align horizontal-align"}
+    >
+      <h1>Simple Counter in React using Hooks</h1>
+      <div
+        id={"counter-wrapper"}
+        className={"flex column vertical-align justify-sp-ev"}
+      >
+        <div className={"flex column vertical-align"}>
+          <p>{count}</p>
+          <div className={"flex row justify-sp-ar mt-10"}>
+            <button className={"mr-5"} onClick={handleSubtractOne}>
+              -1
+            </button>
+            <button onClick={handleAddOne}>+1</button>
+          </div>
+        </div>
+        <div>
+          <h3>Add / substract custom quantity</h3>
+          <div className={"flex row vertical-align horizontal-align"}>
+            <input
+              type="text"
+              value={quantity}
+              onChange={handleOnChange}
+              className={"mr-10"}
+            />
+
+            <button
+              className={"mr-5 width-40"}
+              onClick={handleSubtractQuantity}
+            >
+              -
+            </button>
+            <button className={"width-40"} onClick={handleAddQuantity}>
+              +
+            </button>
+          </div>
+        </div>
+        <button id={"counter-reset"} onClick={handleResetCounter}>
+          Reset Counter
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
